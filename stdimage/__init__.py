9adb53b0c8acd8bfead49b3bfbffe0879b07dc97
Steven Klass <sklass@pointcircle.com>
2012-10-01 14:02:11 -0700

    Added south to geographic.  Should be fun..

--- stdimage/__init__.py
+++ stdimage/__init__.py
@@ -1,1 +1,18 @@
 from fields import StdImageField
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    rules = [
+        (
+            (StdImageField,),
+            [],
+            {
+                "size": ["size", {"default": None}],
+                "thumbnail_size": ["thumbnail_size", {"default": None}],
+                "upload_to": ["upload_to", {"default": None}],
+            },
+        )
+    ]
+    add_introspection_rules(rules, ["^stdimage\.fields"])
+except ImportError:
+    pass
