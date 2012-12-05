0efa2f3443e2dcc6efc4feea9e9247dfb30529c7
Steven Klass <sklass@pointcircle.com>
2012-12-05 14:35:12 -0700

    ......

--- setup.py
+++ setup.py
@@ -1,41 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-import os
-
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-_dir = 'stdimage'
-
-for dirpath, dirnames, filenames in os.walk(_dir):
-    # Ignore PEP 3147 cache dirs and those whose names start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.') or dirname == '__pycache__':
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
+from setuptools import find_packages, setup
 
 setup(
     name='django-stdimage',
@@ -55,6 +20,7 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    packages=packages,
-    data_files=data_files,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_data={'stdimage': ['static/js/*.js', 'templates/stdimage/*.html']},
+    include_package_data=True,
 )
