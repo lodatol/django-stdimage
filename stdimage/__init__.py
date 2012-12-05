c2654994abd25db403c9a5d8973a791247d16089
Steven Klass <sklass@pointcircle.com>
2012-12-05 08:04:18 -0700

    Clean up and give credit

--- stdimage/__init__.py
+++ stdimage/__init__.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""__init__.py: Django apps package stdimage"""
+
+__author__ = 'Marc Garcia'
+__version_info__ = (0, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
+__date__ = '2011/06/22 09:59:51'
+__credits__ = ['Marc Garcia', 'Steven Klass']
+__license__ = 'GNU Lesser General Public License'
+
 from fields import StdImageField
 
 try:
